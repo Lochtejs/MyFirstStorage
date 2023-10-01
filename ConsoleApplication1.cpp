0896b8824a08a9c44d4ec211b3d5b304b00bbf72
Lochtejs <72276545+Lochtejs@users.noreply.github.com>
2023-10-02 00:15:51 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,20 +1,30 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cmath>
+#include <fstream>
+
 using namespace std;
+
 float f(float a)
 {
 	return 1 / ((a) * (a));
 }
+
 int main()
 {
 	float n, summary = 0.0;
+
+	ofstream foo("dots.csv", ios::out);
+
 	for (n = 1.0; n <= 1000; n++)
 	{
 		for (float i = 1.0; i <= n; i++)
 		{
 			summary += f(i);
 		}
-		cout << sqrt(summary * 6) << endl;
+
+		foo << sqrt(summary * 6);
+		foo << " ";
+		foo << n << endl;
 		summary = 0;
 	}
 
