529618a54b2bf6e27891f4de1a8314bf4c26df99
Lochtejs <72276545+Lochtejs@users.noreply.github.com>
2023-10-01 21:34:13 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+float f(float a)
+{
+	return 1 / ((a) * (a));
+}
+int main()
+{
+	float n, summary = 0.0;
+	for (n = 1.0; n <= 1000; n++)
+	{
+		for (float i = 1.0; i <= n; i++)
+		{
+			summary += f(i);
+		}
+		cout << sqrt(summary * 6) << endl;
+		summary = 0;
+	}
+
+	return 0;
+}
